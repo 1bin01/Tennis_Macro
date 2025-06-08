24c1d771d610dd3aa23ebc411914292811a047f1
WONBIN CHO <wonbin0504@naver.com>
2025-06-08 20:01:42 +0900

    Update naegok.py

--- naegok.py
+++ naegok.py
@@ -4,32 +4,80 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 import time
 import datetime
 import requests
-import pyautogui
 
-clickX = 898
-clickY = 482
+### 코트 URL 관리 ###
+bookingUrl = "https://booking.naver.com/booking/10/bizes/217811/items/"
 
-coutList = [
-        "월 내곡 1번코트(하드)",
-        "월 내곡 2번코트(하드)",
-        "월 내곡 3번코트(하드)",
-        "월내곡 4번코트(하드)",
-        "월 내곡 5번코트(하드)",
-        "월 내곡 6번코트(하드)",
-        "월 내곡 7번코트(인조잔디)",
-        "월 내곡 8번코트(인조잔디)"
-    ]
+# Need to modify!!
+# 월에 맞게 코트 URL 번호 갱신해줘야함
+courtUrlNumbers = {
+    6 : {
+        1: "4348531",  # 1번 코트
+        2: "4348532",  # 2번 코트
+        3: "4348535",  # 3번 코트
+        4: "4348536",  # 4번 코트
+        5: "4348537",  # 5번 코트
+        6: "4348539",  # 6번 코트
+        7: "4348540",  # 7번 코트        
+        8: "6503551",  # 8번 코트
+    },
+    7 : {
+        1: "6804184",  # 1번 코트
+        2: "6804189",  # 2번 코트
+        3: "6804192",  # 3번 코트
+        4: "6804195",  # 4번 코트
+        5: "6804221",  # 5번 코트
+        6: "6804227",  # 6번 코트
+        7: "6804230",  # 7번 코트
+        8: "6804234",  # 8번 코트
+    }
+}
+
+def getCourtURL(month, courtNumber):
+    try:
+        return bookingUrl + courtUrlNumbers[month][courtNumber]
+    except KeyError:
+        return "해당 코트의 URL이 존재하지 않습니다."
+###
+
+
+### 입력 받는 함수들 ###
+def getCourtNumber():
+    while True:
+        try:
+            choice = int(input("\n코트를 선택해주세요 (1 ~ 8) :  "))
+            if 1 <= choice <= 8:
+                return choice
+            else:
+                print("\n1 이상 8 이하의 수를 입력해주세요.\n")
+        except ValueError:
+            print("\n숫자를 입력해 주세요.\n")
+
+def getMonth():
+    while True:
+        try:
+            month = int(input("예약할 달을 입력해주세요 (예: 6) : "))
+            if 1 <= month <= 12:
+                return month
+            else:
+                print("\n1 이상 12 이하의 수를 입력해주세요.\n")
+        except ValueError:
+            print("\n숫자를 입력해 주세요.\n")
 
 def getDay():
     while True:
-        day = int(input("예약일을 입력해주세요 (예: 21): "))
-        if 1 <= day <= 31:
-            return day
-        else:
-            print("\n1부터 31 사이의 정수를 입력해주세요.\n")
+        try:
+            day = int(input("예약일을 입력해주세요 (예: 21): "))
+            if 1 <= day <= 31:
+                return day
+            else:
+                print("\n1부터 31 사이의 정수를 입력해주세요.\n")
+        except ValueError:
+            print("\n숫자를 입력해 주세요.\n")
             
             
 def getHours():
@@ -44,167 +92,153 @@ def getHours():
         except:
             print("\n형식을 지켜주세요. (예: 12-14)\n")
 
-
-def chooseCourt():
-    while True:
-        try:
-            choice = int(input("코트를 선택해 주세요 (하드는 1, 인조잔디는 2, 상관없으면 3 을 입력) : "))
-            if 1 <= choice <= 3:
-                return choice
-            else:
-                print("\n1(하드) 또는 2(인조잔디) 또는 3(하드 + 인조잔디)를 입력해주세요.\n")
-        except ValueError:
-            print("\n숫자를 입력해 주세요.\n")
             
-def getCandidate():
+def getOption():
     date = getDay()
     print()
     timeslot = getHours()
     print()
-    court = chooseCourt()
-    print()
-    return (date, timeslot, court)
+    return (date, timeslot)
 
 def printOption(option):
-    date, timeslot, court = option        
-    courtType = "하드 + 인조잔디"
-    if court == 1:
-        courtType = "하드"
-    elif court == 2:
-        courtType = "인조잔디"
-    print(f'{month}월 {date}일 {timeslot[0]}-{timeslot[-1] + 1}시에 {courtType}를 예약하겠습니다\n')
+    date, timeslot = option        
+    print(f'{month}월 {date}일 {timeslot[0]}-{timeslot[-1] + 1}시에 예약하겠습니다\n')
     
 
-def getCourtOptions():
-    courtOptions = []
+def getBookingOptions():
+    bookingOptions = []
     print('------------------------------------')
     idx = 0
     while(1):
         idx += 1
         print(f'\n {idx}번째 코트 예약을 도와드리겠습니다.\n')
 
-        option = getCandidate()
-        courtOptions.append(option)
+        option = getOption()
+        bookingOptions.append(option)
         printOption(option)
         
         inp = 0
         while True:
-            inp = int(input("추가로 입력하려면 1, 아니면 0을 입력해주세요 : "))
-            if inp == 0 or inp == 1: break        
-        if inp == 0: break
+            try:
+                inp = int(input("추가로 입력하려면 1, 아니면 0을 입력해주세요 : "))
+                if inp == 0 or inp == 1:
+                    break
+                else:
+                    print("\n0 또는 1을 입력해주세요.\n")
+            except ValueError:
+                print("\n숫자를 입력해 주세요.\n")
         
+        if inp == 0:
+            break
         print('-------------------------------------')
         
         
     # 예약 후보 목록
     print('\n------------ 예약 후보 목록 -------------\n')
-    for date, timeslot, court in courtOptions:
-        courtType = "하드 + 인조잔디"
-        if court == 1:
-            courtType = "하드"
-        elif court == 2:
-            courtType = "인조잔디"
-            
-        print(f'{month}월 {date}일 {timeslot[0]}시-{timeslot[-1] + 1}시 {courtType}')
+    for date, timeslot in bookingOptions:
+        print(f'{month}월 {date}일 {timeslot[0]}시-{timeslot[-1] + 1}시')
         
     print('\n-------------------------------------\n')
-    return courtOptions
+    return bookingOptions
 
+def selectDay(day):
+    try:
+        xpath = f'//button[@class="calendar_date" and .//span[text()="{day}"]]'
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, xpath))
+        ).click()
+        return 1
+    except TimeoutException as e:
+        print("날짜 선택에 실패했습니다.", e)
+        return 0
 
-# Selenium WebDriver 설정
-
-def findCourt():
-    originalURL = driver.current_url
-    pyautogui.moveTo(clickX, clickY) 
-    for i in range(20):
-        pyautogui.click()
-        if driver.current_url != originalURL:
-            return 1
-        time.sleep(0.3)
-    return 0
-
-def make_reservation(day, timeslot, court):
-    # 1. 날짜 선택
-    xpath = f'//button[@class="calendar_date" and .//span[text()="{day}"]]'
-    WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, xpath))
-    ).click()
-
-    # 2. 원하는 시간대 선택
-    WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "li.time_item"))
-    )
-    time_items = driver.find_elements(By.CSS_SELECTOR, "li.time_item")
-    
-    Time = 6
-    for item in time_items:
-        if Time in timeslot:
-            if "disabled" not in item.get_attribute("class"):
-                item.find_element(By.TAG_NAME, "button").click()
-            else :
-                print(f'{Time}시 자리가 없습니다 ㅠㅠ')
-                return 0
-        Time += 1
-    
-    WebDriverWait(driver, 10).until( # 적용 버튼
-        EC.element_to_be_clickable((
-            By.XPATH, '//button[contains(text(), "적용")]'
-        ))
-    ).click()
-    
-    # 3. 코트 선택    
-    if findCourt() == False:
-        print("만족하는 코트가 없습니다 ㅠㅠ")
+def Select(timeslot):
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "li.time_item"))
+        )
+        time_items = driver.find_elements(By.CSS_SELECTOR, "li.time_item")
+        
+        Time = 6
+        for item in time_items:
+            if Time in timeslot:
+                if "disabled" not in item.get_attribute("class"):
+                    item.find_element(By.TAG_NAME, "button").click()
+                else :
+                    print(f'{Time}시 자리가 없습니다 ㅠㅠ')
+                    return 0
+            Time += 1
+    except Exception as e:
         return 0
     
-    WebDriverWait(driver, 20).until(  # 다음 버튼
-        lambda d: (btn := d.find_element(By.XPATH, '//button[text()="다음"]')) 
-                and "disabled" not in btn.get_attribute("class") 
-                and btn.is_enabled() 
-                and btn.is_displayed() and btn
-    ).click()
-
+    return 1
     
-    # 4. 동의 및 결제
-    WebDriverWait(driver, 20).until(  # 동의하고 결제하기 버튼
-        lambda d: (btn := d.find_element(By.XPATH, '//button[text()="동의하고 결제하기"]')) 
-                and "disabled" not in btn.get_attribute("class") 
-                and btn.is_enabled() 
-                and btn.is_displayed() and btn
-    ).click()
+def selectTimeSlot(timeslot):
+    if Select(timeslot) == 1:
+        return 1
+    else:
+        try:
+            WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "slick-next"))
+            ).click()
+            return Select(timeslot)      
+        except Exception as e2:
+            print("넘어가기 버튼 실패 ㅠ")  
+            return 0
+    
+        
+def tryBooking(day, timeslot):
+    # 1. 날짜 선택
+    if selectDay(day) == False:
+        return 0
+
+    # 2. 원하는 시간대 선택
+    if selectTimeSlot(timeslot) == False:
+        return 0
+    
+    # # 3. 적용 버튼
+    # try:
+    #     WebDriverWait(driver, 50).until( 
+    #         EC.element_to_be_clickable((
+    #             By.XPATH, '//button[contains(text(), "적용")]'
+    #         ))
+    #     ).click()
+    # except Exception as e:
+    #     print("적용 버튼 클릭 실패:", e)
+    #     return 0    
+    
+    # 4. 다음 버튼
+    try:
+        WebDriverWait(driver, 10)( 
+            lambda d: (btn := d.find_element(By.XPATH, '//button[text()="다음"]')) 
+                    and "disabled" not in btn.get_attribute("class") 
+                    and btn.is_enabled() 
+                    and btn.is_displayed() and btn
+        ).click()
+    except Exception as e:
+        print("다음 버튼 클릭 실패:", e)
+        return 0
+    
+
+    # 5. 동의 및 결제
+    try:
+        WebDriverWait(driver, 10).until( 
+            lambda d: (btn := d.find_element(By.XPATH, '//button[text()="동의하고 결제하기"]')) 
+                    and "disabled" not in btn.get_attribute("class") 
+                    and btn.is_enabled() 
+                    and btn.is_displayed() and btn
+        ).click()
+    except Exception as e:
+        print("동의 및 결제 버튼 클릭 실패:", e)
+        return 0    
+    
     return 1
 
 
-def run():
-    # 예약 가능한 코트 찾기
-    for date, timeslot, court in courtOptions:
-        driver.get("https://booking.naver.com/booking/10/bizes/217811")
-        
-        # 6. 캘린더로 이동
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "booking_select"))
-        ).click()
-
-        # 7. 달 맞추기
-        while True:
-            WebDriverWait(driver, 10).until(
-                EC.text_to_be_present_in_element((By.CLASS_NAME, "calendar_title"), ".")
-            )
-            current_month = int(WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "calendar_title"))
-            ).text.strip().split(".")[1])
-            
-            if(current_month == month) : break
-            
-            WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.CLASS_NAME, "btn_next"))
-            ).click()
-            print(f"현재 달은 {current_month}월입니다. 다음 달로 넘어갑니다.")
-            
-        print(f"현재 달력에서 달은 {current_month}입니다")
-        
-        # 8. 예약 시도
-        if make_reservation(date, timeslot, court):
+def run(courtUrl):
+    for date, timeslot in bookingOptions:
+        driver.get(courtUrl)
+        if tryBooking(date, timeslot):
             return 1
     return 0
 
@@ -215,7 +249,7 @@ def waitFunction(url):
     while(True):
         serverTime = getServerTime(url)
         print(serverTime)
-        if serverTime.second % 60 < 5:
+        if serverTime.second % 60 < 10:
             break
 
 '''
@@ -223,8 +257,6 @@ Todo
 1. python 설치 (3.13 기준)
 2. 컴퓨터에 selenium 설치 (터미널에서 명령어 입력 : pip install selenium)
    컴퓨터에 requests 설치 (터미널에서 명령어 입력 : pip install requests)
-   컴퓨터에 pyautogui 설치 (터미널에서 명령어 입력 : pip install pyautogui)
-3. mouse_location.py를 실행해서 clickX, clickY 좌표 저장
 4. 네이버 로그인
 5. 화면에 따라 작동이 잘 안될 수도 있으니 꼭 테스트해보기
 
@@ -233,37 +265,36 @@ Todo
 
 tmp = input('\n아무거나 입력해주세요 : ')
 
-print('--------------macro loaded--------------------')
+print('\n--------------macro loaded--------------------\n')
 
-# 1. 예약 달 입력받기
-month = int(input("\n몇월에 예약하시겠습니까? "))
-print(f"\n{month}월 예약 도와드리겠습니다\n") 
+# 1. 예약 달과 코트 번호 입력받기
+month = getMonth()
+courtNumber = getCourtNumber()
+print(f"\n{month}월 {courtNumber}번 코트 예약 도와드리겠습니다\n")
+courtUrl = getCourtURL(month, courtNumber)
 
-
-# 2. 예약 날짜 입력받기
-coutList = [court.replace("월", f"{month}월") for court in coutList]
-courtOptions = getCourtOptions() # (날짜, 시간, 코트)
-
+# 2. 예약 날짜와 시간 입력받기
+bookingOptions = getBookingOptions() # (날짜, 시간)
 
 # 3. 크롬 드라이버 설정
 options = Options()
 options.add_argument("--start-maximized")
 driver = webdriver.Chrome(options=options)
 
-
 # 4. 로그인
 driver.get("https://nid.naver.com/nidlogin.login")
 
 # # 네이버 예약 페이지 열기
 WebDriverWait(driver, 50).until(EC.url_changes("https://nid.naver.com/nidlogin.login"))
-driver.get("https://booking.naver.com/booking/10/bizes/217811")
+driver.get(courtUrl)
 
 
 #  5. 00초마다 코트 예약 시도
 while(True):
-    waitFunction('https://booking.naver.com/booking/10/bizes/217811')
-    if run() :
+    waitFunction(courtUrl)
+    if run(courtUrl):
         break
+    time.sleep(10)
 
 
 for i in range(60, 0, -1):
